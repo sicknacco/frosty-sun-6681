b66b6868f695cfd7dd9e4bc5ac667a63c1f66e43
Nick Sacco <nicksacco10@gmail.com>
2023-04-24 09:36:24 -0700

    test: Setup doctor show page tests for us1

---
+++ spec/features/doctors/show_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe " '/doctors/:id' Doctor Show Page", type: :feature do
+  before(:each) do
+    test_data
+  end
+
+  describe "When I visit a doctor's show page I see all of that doctor's information" do
+    it 'shows doc name, specialty, university, hospital name, and patients names' do
+      visit "/doctors/#{@doc1}"
+
+      expect(page).to have_content("Dr. Faber")
+      expect(page).to have_content("Rheumatology")
+      expect(page).to have_content("U.O.I.")
+
+      expect(page).to have_content("That Big One")
+      expect(page).to have_content(@patient1.name)
+      expect(page).to have_content(@patient2.name)
+      expect(page).to have_content(@patient3.name)
+      expect(page).to_not have_content(@patient4.name)
+    end
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,7 @@
 def test_data
   @hospital = Hospital.create!(name: "That Big One")
 
-  @doc1 = @hospital.doctors.create!(name: "Dr. Faber", specialty: "Rheumatology", university: "UOI")
+  @doc1 = @hospital.doctors.create!(name: "Dr. Faber", specialty: "Rheumatology", university: "U.O.I.")
 
   @patient1 = Patient.create!(name: "Jim Jimson", age: 56)
   @patient2 = Patient.create!(name: "Tim Timerson", age: 83)
