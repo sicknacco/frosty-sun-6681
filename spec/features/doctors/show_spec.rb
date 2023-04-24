3d769ad9c392cfc0d0edca4332c183081e17ee46
Nick Sacco <nicksacco10@gmail.com>
2023-04-24 10:48:35 -0700

    test: beginning tests for us2

--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -12,12 +12,22 @@ RSpec.describe " '/doctors/:id' Doctor Show Page", type: :feature do
       expect(page).to have_content("Dr. Faber")
       expect(page).to have_content("Rheumatology")
       expect(page).to have_content("U.O.I.")
-
+      
       expect(page).to have_content("That Big One")
       expect(page).to have_content(@patient1.name)
       expect(page).to have_content(@patient2.name)
       expect(page).to have_content(@patient3.name)
       expect(page).to_not have_content(@patient4.name)
+    end
+    
+    it 'has a button next to patient names to remove patient' do
+      visit doctor_path(@doc1)
+      
+      within "#doc_patients_#{@patient1}"
+      expect(page).to have_button("Delete Patient")
+      click_button("Delete Patient")
+      expect(current_path).to eq(doctor_path(@doc1))
+      end
     end
   end
 end
