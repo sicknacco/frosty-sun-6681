56daa30085605f2af4d9f1b5f4d60ad1e5f5763a
Nick Sacco <nicksacco10@gmail.com>
2023-04-24 08:49:48 -0700

    fix migrations for story1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_24_153324) do
+ActiveRecord::Schema.define(version: 2023_04_24_151527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,12 +34,6 @@ ActiveRecord::Schema.define(version: 2023_04_24_153324) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["doctor_id"], name: "index_patients_on_doctor_id"
-  end
-
-  create_table "universities", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "doctors", "hospitals"
